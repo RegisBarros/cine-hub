a57eea1214f030644f74e15494fddda467232801
Regis <regisbarros21@gmail.com>
2024-01-29 22:02:44 -0300

    create microservices movie info services

---
+++ movies-info-service/src/main/java/com/cine/repository/MovieInfoRepository.java
@@ -0,0 +1,9 @@
+package com.cine.repository;
+
+import com.cine.domain.MovieInfo;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieInfoRepository extends ReactiveMongoRepository<MovieInfo, String> {
+}
---
+++ movies-info-service/src/test/java/com/cine/integration/repository/MovieInfoRepositoryIntegrationTest.java
@@ -0,0 +1,69 @@
+package com.cine.integration.repository;
+
+import com.cine.domain.MovieInfo;
+import com.cine.repository.MovieInfoRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.ActiveProfiles;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataMongoTest
+@ActiveProfiles("test")
+public class MovieInfoRepositoryIntegrationTest {
+
+    @Autowired
+    MovieInfoRepository movieInfoRepository;
+
+    @BeforeEach
+    void setUp() {
+        var movie1 = new MovieInfo(null, "Batman Begins", 2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15"));
+        var movie2 = new MovieInfo(null, "The Dark Knight", 2008, List.of("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18"));
+        var movie3 = new MovieInfo("abc", "The Dark Knight Rises", 2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20"));
+
+        movieInfoRepository.saveAll(List.of(movie1, movie2, movie3))
+                .blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        movieInfoRepository.deleteAll().block();
+    }
+
+    @Test
+    void findAll() {
+
+        // when
+        Flux<MovieInfo> movieInfoFlux = movieInfoRepository.findAll();
+
+        // then
+        StepVerifier.create(movieInfoFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    void findById() {
+
+        // when
+        Mono<MovieInfo> movieInfoMono = movieInfoRepository.findById("abc");
+
+        // then
+        StepVerifier.create(movieInfoMono)
+                .assertNext(movieInfo -> {
+                    assertThat(movieInfo.getName()).isEqualTo("The Dark Knight Rises");
+                    assertThat(movieInfo.getYear()).isEqualTo(2012) ;
+                    assertThat(movieInfo.getReleaseDate()).isEqualTo(LocalDate.parse("2012-07-20")) ;
+                })
+                .verifyComplete();
+    }
+}
